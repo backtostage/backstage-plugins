b09b58d94aef7751cb9f73555b0c3cc99cd497be
Rogerio Angeliski <rogerio.angeliski@rdstation.com>
2024-09-20 14:08:58 -0300

    update query param to list projects

--- plugins/catalog-backend-module-gcp/src/lib/resourcemanager.ts
+++ plugins/catalog-backend-module-gcp/src/lib/resourcemanager.ts
@@ -1,17 +1,12 @@
 import {ProjectsClient} from '@google-cloud/resource-manager';
 
-export async function listProjects(parent?: string) {
+export async function listProjects(query?: string) {
     const resourcemanagerClient = new ProjectsClient();
     const request = {
-        query: ""
+        query
       };
-      
-      if(parent) {
-        request.query=`parent:organizations/${parent}`;
-      }
 
-      //we don't have to handle pagination here, the client will do it for us
+      // we don't have to handle pagination here, the client will do it for us
       const result = await resourcemanagerClient.searchProjects(request);
-
       return result[0];
 }
